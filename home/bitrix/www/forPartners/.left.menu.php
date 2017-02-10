db548277f16e99131e21ded3a10bcb692d0f98b8
Andrew Knyazev <akanew@mail.ru>
2017-02-10 20:18:48 +0400

    Создание разделов и страниц

---
+++ home/bitrix/www/forPartners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/forPartners/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/forPartners/sheduleOfEvents/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/forPartners/howToBecomePartner.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
