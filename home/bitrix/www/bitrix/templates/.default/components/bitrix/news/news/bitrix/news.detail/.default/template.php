74d1bff6f532e5fc913b13684ad5d72baf10180a
Andrew Knyazev <akanew@mail.ru>
2017-02-18 23:02:20 +0400

    Добавил компонент - новости

---
+++ home/bitrix/www/bitrix/templates/.default/components/bitrix/news/news/bitrix/news.detail/.default/template.php
@@ -0,0 +1,14 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$this->setFrameMode(true);
+?>
+
+<div class="main_post">
+	<div class="main_title">
+		<p class="title"><?=$arResult["NAME"]?></p><span class="main_date"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
+	</div>
+
+	<div class="ps_content">
+		<img src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>" align="left" alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"/>
+			<?=$arResult["DETAIL_TEXT"];?>
+	</div>
+</div>
