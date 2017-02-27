3756717c846e48df2df6dbec63207aaa103cd571
Andrew Knyazev <akanew@mail.ru>
2017-02-27 14:55:09 +0400

    Добавил событие для отправки всем пользователям группы *Контент-менеджеры* почтовых сообщений, при добавлении нового пользователя в эту группу

--- home/bitrix/www/bitrix/php_interface/constants.php
+++ home/bitrix/www/bitrix/php_interface/constants.php
@@ -3,4 +3,5 @@
 	define("ADMIN_GROUP_ID", 1);
 	define("PRODUCTS_IBLOCK_ID", 2);
 	define("NEWS_IBLOCK_ID", 1);
+	define("CONTENT_MANAGER_GROUP_ID", 5);
 ?>
--- home/bitrix/www/bitrix/php_interface/events.php
+++ home/bitrix/www/bitrix/php_interface/events.php
@@ -1,4 +1,6 @@
 <?
+global $APPLICATION;
+
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("IBlock_Elements_Update", "OnBeforeIBlockElementUpdateHandler"));
 
 class IBlock_Elements_Update
@@ -31,7 +33,7 @@ class IBlock_Elements_Delete
     function OnBeforeIBlockElementDeleteHandler($ID)
     {
 		if(CModule::IncludeModule('iblock'))
-			{
+		{
 			$arFilter = Array("IBLOCK_ID"=>PRODUCTS_IBLOCK_ID, "ID" => $ID);
 			$res = CIBlockElement::GetList(Array("SORT"=>"ASC"), $arFilter, false, false, Array("ID", "SHOW_COUNTER"));
 			while($ar_fields = $res->GetNext())
@@ -39,8 +41,70 @@ class IBlock_Elements_Delete
 					$APPLICATION->throwException("Не стоит удалять товар, количество просмотров которого составляет ".$ar_fields[SHOW_COUNTER].'.');
 					return false;
 				}
-					
-			}
+		}
     }
+}
+
+AddEventHandler("main", "OnBeforeUserAdd", Array("Add_User_To_Group_Manager", "OnBeforeUserAddHandler"));
+AddEventHandler("main", "OnBeforeUserUpdate", Array("Add_User_To_Group_Manager", "OnBeforeUserUpdateHandler"));
+
+class Add_User_To_Group_Manager
+{
+    function OnBeforeUserAddHandler(&$arFields){
+		//sendPostAllUsers(&$arFields);
+		foreach($arFields["GROUP_ID"] as $arGroup){			
+			if($arGroup["GROUP_ID"] == CONTENT_MANAGER_GROUP_ID){
+				$arUsers = CGroup::GetGroupUser($arGroup["GROUP_ID"]);
+				foreach($arUsers as $arUser){
+					$arFilter = Array("GROUPS_ID" => Array($arGroup["GROUP_ID"]));
+					$rsUsers = CUser::GetList(($by="personal_country"), ($order="desc"), $arFilter);
+					$arEmail = array();
+					while($arResUser = $rsUsers->GetNext())
+						$arEmail[] = $arResUser["EMAIL"];
+				}
+			}
+		}
+		
+		if(count($arEmail) > 0)
+		{
+			$arEventFields = array(
+				"MAIL_TO" => implode(", ", $arEmail),
+				"USER" => $arFields["LAST_NAME"].' '.$arFields["NAME"]
+			);
+			CEvent::Send("ADD_USER_TO_MANAGER_GROUP", "s1", $arEventFields);
+		}
+		$APPLICATION->throwException("Этого пользователя в группу \"Контент-менеджер\" позже добавит администратор.");
+		//return false;
+    }
+	
+	function OnBeforeUserUpdateHandler(&$arFields){
+		//sendPostAllUsers(&$arFields)
+		foreach($arFields["GROUP_ID"] as $arGroup){			
+			if($arGroup["GROUP_ID"] == CONTENT_MANAGER_GROUP_ID){
+				$arUsers = CGroup::GetGroupUser($arGroup["GROUP_ID"]);
+				foreach($arUsers as $arUser){
+					$arFilter = Array("GROUPS_ID" => Array($arGroup["GROUP_ID"]));
+					$rsUsers = CUser::GetList(($by="personal_country"), ($order="desc"), $arFilter);
+					$arEmail = array();
+					while($arResUser = $rsUsers->GetNext())
+						$arEmail[] = $arResUser["EMAIL"];
+				}
+			}
+		}
+		
+		if(count($arEmail) > 0)
+		{
+			$arEventFields = array(
+				"MAIL_TO" => implode(", ", $arEmail),
+				"USER" => $arFields["LAST_NAME"].' '.$arFields["NAME"]
+			);
+			CEvent::Send("ADD_USER_TO_MANAGER_GROUP", "s1", $arEventFields);
+		}
+		$APPLICATION->throwException("Этого пользователя в группу \"Контент-менеджер\" позже добавит администратор.");
+		//return false;
+	}
+	
+	/*function sendPostAllUsers(&$arFields){}*/
+	
 }
 ?>
